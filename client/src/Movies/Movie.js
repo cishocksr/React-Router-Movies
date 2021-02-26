14f9a2f1eb9173682bc15562e7a600d750c23fa9
Christopher Shockley <cishockleysr@gmail.com>
2021-02-26 01:30:03 -0500

    refactor with MovieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-const Movie = props => {
+const Movie = (props) => {
   const [movie, setMovie] = useState();
 
   useEffect(() => {
-    const id = props.match.params.id
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
+      .then((response) => {
         setMovie(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, [props.match.params.id]);
@@ -30,30 +31,7 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-      <Link to={`/movies/${movie.id}`}>
-        <h2>{title}</h2>
-        </Link>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  <MovieCard />;
 };
 
 export default Movie;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-const MovieList = props => {
+const MovieList = (props) => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get("http://localhost:5000/api/movies")
-        .then(response => {
-          console.log(response.data)
+        .get('http://localhost:5000/api/movies')
+        .then((response) => {
+          console.log(response.data);
           setMovies(response.data);
         })
-        .catch(error => {
-          console.error("Server Error", error);
+        .catch((error) => {
+          console.error('Server Error', error);
         });
     };
 
@@ -21,8 +22,8 @@ const MovieList = props => {
   }, []);
 
   return (
-    <div className="movie-list">
-      {movies.map(movie => (
+    <div className='movie-list'>
+      {movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -30,27 +31,7 @@ const MovieList = props => {
 };
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`movies/${movie.id}`}>
-      <div to="/movies.:id" className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-    </Link>
-  );
+  <MovieCard />;
 }
 
 export default MovieList;
